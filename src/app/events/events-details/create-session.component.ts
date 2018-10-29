e993226e11c281bc627204b319f71fd10a5d66df
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-10-28 22:34:20 -0700

    Add form validators to create-session

--- src/app/events/events-details/create-session.component.ts
+++ src/app/events/events-details/create-session.component.ts
@@ -1,12 +1,19 @@
-import { IEvent, ISession } from './../shared/event.model';
+import { restrictedWords, ISession } from '../shared/index';
 import { Component, OnInit } from "@angular/core";
-import {FormControl,FormGroup,Validators} from '@angular/forms'
-
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
-    templateUrl: './create-session.component.html'
+    templateUrl: './create-session.component.html',
+    styles: [`
+    em {float:right; color:#e05c65; padding-left:10px;}
+    .error input, .error select, .error textarea {background-color:#e3c3c5; }
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color: #999; }
+    .error :-ms-input-placeholder { color: #999; }
+  `]
 })
-export class CreateSessionComponent implements OnInit{
+export class CreateSessionComponent implements OnInit {
     newSessionForm: FormGroup
     name: FormControl
     presenter: FormControl;
@@ -14,32 +21,34 @@ export class CreateSessionComponent implements OnInit{
     level: FormControl;
     abstract: FormControl;
 
-        ngOnInit(): void{
-            this.name = new FormControl('', Validators.required)
-            this.presenter = new FormControl('', Validators.required)
-            this.duration = new FormControl('', Validators.required)
-            this.level = new FormControl('', Validators.required)
-            this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+    ngOnInit(): void {
+        this.name = new FormControl('', Validators.required)
+        this.presenter = new FormControl('', Validators.required)
+        this.duration = new FormControl('', Validators.required)
+        this.level = new FormControl('', Validators.required)
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo','bar'])])
 
-            this.newSessionForm = new FormGroup({
-                name: this.name,
-                presenter: this.presenter,
-                duration: this.duration,
-                level: this.level,
-                abstract: this.abstract
-            })
+        this.newSessionForm = new FormGroup({
+            name: this.name,
+            presenter: this.presenter,
+            duration: this.duration,
+            level: this.level,
+            abstract: this.abstract
+        })
+    }
+
+    saveSession(formValues): void {
+        let session: ISession = {
+            id: undefined,
+            name: formValues.name,
+            presenter: formValues.presenter,
+            duration: +formValues.duration,
+            level: formValues.level,
+            abstract: formValues.abstract,
+            voters: []
         }
 
-        saveSession(formValues): void{
-            let session: ISession = {
-                id: undefined,
-                name: formValues.name,
-                presenter: formValues.presenter,
-                duration: +formValues.duration,
-                level: formValues.level,
-                    abstract: formValues.abstract,
-                    voters: []
-
-            }
-        }
+        console.log(session)
+        //todo: save this session
+    }
 }
